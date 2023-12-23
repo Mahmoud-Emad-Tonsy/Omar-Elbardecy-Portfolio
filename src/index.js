5dafbeeacdd1a3044371da4aaff0cdea3e70599c
MahmoudTonsy <m_tonsy@yahoo.com>
2023-12-23 20:54:44 +0200

    sec commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import ThemeContextProvider from './contexts/ThemeContext'
 
 
@@ -17,4 +17,4 @@ ReactDOM.render(
 );
 
 
-reportWebVitals();
+// reportWebVitals();
